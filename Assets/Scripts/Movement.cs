baf7bf12bbee26328b106129d39d70c614b9fe53
HeartAttackCat <hunter.lane@student.nmt.edu>
2024-02-23 12:03:21 -0700

    Car movement mostly works, just needs reifnement on values and breaking implemented.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,28 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/**
+ * @Author Hunter Lane
+ * @Description This enables the movement for the car.
+ **/
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D body2D;
-    public float verticalSpeed = 0.1f;
-    public float horizontalForce = 0.2f;
+
+    /**
+     * These variables are used to control the coeffcients of the car.
+     * To modify these values click on the car object and go to inspect.
+     * Then change values.
+     **/
+
+    //Coeffciients for the accerations.
+    public float breakCoeffcient = 0.0f;
+    public float speedCoeffcient = 0.0f;
+    public float horizontalCoeffcient = 0.0f;
+
+    //The speed the car starts with.
+    public float startSpeed = 0.0f;
+
 
     // Start is called before the first frame update
     private void Start()
     {
-
+        horizontalCoeffcient = 1 / horizontalCoeffcient;
+        //Add the start speed to the vehicle.
+        body2D.AddForce(new Vector2(0.0f, 32f));
     }
 
-    // Update is called once per frame
-    private void Update()
-    {
-        
-    }
 
     public void FixedUpdate()
     {
-        float HorizontalControl = Input.GetAxis("Horizontal");
-        Vector2 force = new Vector2(HorizontalControl * verticalSpeed, horizontalForce);
-        body2D.AddForce(force);
+        //Apply the user input as a force to the car.
+        float veticalControl = Input.GetAxis("Vertical");
+        float horizontalControl = Input.GetAxis("Horizontal");
+        body2D.AddForce(new Vector2(horizontalControl * horizontalCoeffcient, veticalControl * speedCoeffcient));
+
+        //Apply the vertical friciton to slow down the car in the vertical axis.
+        Vector2 current_Velocity = body2D.velocity;
+        body2D.AddForce(new Vector2(-current_Velocity.x * 0.4f, 0.0f));
     }
 }
